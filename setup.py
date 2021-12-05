da2553419dfbb3d0d6165c2ec987f65a8acb3872
Vopaaz <liyifan945@163.com>
2021-12-05 02:02:30 -0500

    Add CD script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open(r"README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="checkpointing",
+    version="0.0.1",
+    author="Vopaaz",
+    author_email="liyifan945@gmail.com",
+    url="https://github.com/Vopaaz/checkpointing",
+    description="WIP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
