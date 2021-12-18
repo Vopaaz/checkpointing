d239e0bb5382bbe1d92e36ef48934b33e169c70c
Vopaaz <liyifan945@163.com>
2021-12-18 00:01:35 -0500

    Add

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 apidoc:
 	pdoc checkpointing -o ./docs/api --docformat google
+
+test:
+	nosetests --with-coverage --cover-package=checkpointing
--- checkpointing/decorator/base.py
+++ checkpointing/decorator/base.py
@@ -1,16 +1,41 @@
-from typing import Callable, TypeVar, List, Dict
+from typing import Callable, TypeVar, List, Dict, Tuple, Union
 from abc import abstractmethod, ABC
-from checkpointing.base.exceptions import CheckpointNotExist
-
-Identifier = TypeVar("Identifier")
-"""
-Object that uniquely identifies the parameters and dependencies of a function call
-"""
+from checkpointing.decorator.exceptions import CheckpointNotExist
+import inspect
 
 ReturnValue = TypeVar("ReturnValue")
 """
 Return value of the function
 """
+
+
+class Context:
+    """
+    Context of the function call.
+    """
+
+    def __init__(self, args, kwargs, func) -> None:
+        """
+        Args:
+            args: the non-keywords arguments of the function call
+            kwargs: the keyword arguments of the function call
+            func: the function object that is being called
+        """
+
+        self.__args: List = args
+        """Arguments of the function call"""
+
+        self.__kwargs: Dict = kwargs
+        """Keyword arguments of the function call"""
+
+        self.__func: Callable[..., ReturnValue] = func
+        """Function called"""
+
+    @property
+    def arguments(self):
+        signature = inspect.signature(self.__func)
+        bound_args = signature.bind(*self.__args, **self.__kwargs)
+        return bound_args.arguments
 
 
 class DecoratorCheckpoint(ABC):
@@ -27,55 +52,27 @@ class DecoratorCheckpoint(ABC):
                 - `"ignore"`, the exception will be ignored and the user function will be invoked and executed normally.
         """
 
-        self.args: List = []
-        """Arguments in the latest function call"""
-
-        self.kwargs: Dict = {}
-        """Keyword arguments in the latest function call"""
-
-        self.func: Callable[..., ReturnValue] = None
-        """Function to be decorated"""
-
         self.error: str = error
         """The behavior when identification, saving or retrieval raises unexpected exceptions."""
 
     def __call__(self, func: Callable[..., ReturnValue]) -> Callable[..., ReturnValue]:
         """Magic method invoked when used as a decorator."""
 
-        self.func = func
-
         def inner(*args, **kwargs) -> ReturnValue:
-            self.args = args
-            self.kwargs = kwargs
-
+            ctx = Context(args=args, kwargs=kwargs, func=func)
             try:
-                id = self.identify()
-            except Exception as e:
-                pass
-            
-            try:
-                res = self.retrieve(id)
+                res = self.retrieve(ctx)
             except CheckpointNotExist:
                 res = func(*args, **kwargs)
-                self.save(res, id)
-
+                self.save(ctx, res)
             return res
 
         return inner
 
     @abstractmethod
-    def identify(self) -> Identifier:
+    def save(self, ctx: Context, result: ReturnValue) -> None:
         pass
 
     @abstractmethod
-    def save(self, id: Identifier, result: ReturnValue) -> None:
-        pass
-
-    @abstractmethod
-    def retrieve(self, id: Identifier) -> ReturnValue:
-        pass
-
-
-class BuiltinDecoratorCheckpoint(DecoratorCheckpoint):
-    def __init__(self) -> None:
+    def retrieve(self, ctx: Context) -> ReturnValue:
         pass
