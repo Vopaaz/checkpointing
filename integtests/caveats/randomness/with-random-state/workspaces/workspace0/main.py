994d4a2543494dbefae9720ca52be2101ef2fcdb
Vopaaz <liyifan945@163.com>
2022-07-24 17:59:50 -0400

    Increase gap for data in integtest

--- integtests/caveats/randomness/with-random-state/workspaces/workspace0/main.py
+++ integtests/caveats/randomness/with-random-state/workspaces/workspace0/main.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LogisticRegression
 from checkpointing import checkpoint
 
 def build_model():
-    X = [[0], [1], [2], [3]]
+    X = [[0], [1], [10], [11]]
     y = [0, 0, 1, 1]
     model = LogisticRegression(solver="saga", random_state=42)
     return model.fit(X, y) 
@@ -10,7 +10,7 @@ def build_model():
 @checkpoint()
 def predict(model):
     print("Running")
-    X = [[0], [1], [2], [3]]
+    X = [[0], [1], [10], [11]]
     return model.predict(X).tolist()
 
 if __name__ == "__main__":
