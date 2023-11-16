edf1d55775bb535662de41d634fc5ae92557a14d
Vopaaz <liyifan945@gmail.com>
2023-11-15 18:54:39 -0800

    Remove unused test

--- tests/util/pickle_test.py
+++ tests/util/pickle_test.py
@@ -3,10 +3,6 @@ from testutils import rmdir_after, tmpdir, mkdir_before
 from pytest import raises
 from checkpointing.config import defaults
 
-def test_too_high_protocol_raises_error():
-    with raises(RuntimeError):
-        pickle.get_pickle_module(6)
-
 def test_dump_load_file(mkdir_before, rmdir_after):
     protocol = defaults["cache.pickle_protocol"]
 
